8c20aa62edd3de5927d4a5aca04db236d3b13ca3
Lachlan <mcwilliam01@gmail.com>
2021-03-11 17:13:34 +0000

    First mutation, added Speaker model and initial query

---
+++ ConferenceGraphQL.Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ConferenceGraphQL.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options)
+        {
+        }
+
+        public DbSet<Speaker> Speakers { get; set; }
+    }
+}
--- ConferenceGraphQL/Startup.cs
+++ ConferenceGraphQL/Startup.cs
@@ -1,21 +1,36 @@
+using ConferenceGraphQL.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ConferenceGraphQL.GraphQL;
+using HotChocolate;
 
 namespace ConferenceGraphQL
 {
     public class Startup
     {
+
+        private readonly IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(
+                options => options.UseSqlServer(Configuration.GetConnectionString("DbConnection")));
+
+            services
+                .AddGraphQLServer()
+                .AddQueryType<Query>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,9 +45,9 @@ namespace ConferenceGraphQL
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
+                app.UseEndpoints(endpoints =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    endpoints.MapGraphQL();
                 });
             });
         }
